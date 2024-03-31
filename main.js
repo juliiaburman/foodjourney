d31fa9dc18489cdb581e13aad8544e6d5ceb25e6
juliiaburman <buju23nb@student.ju.se>
2024-03-31 15:08:32 +0200

    to top button

---
+++ main.js
@@ -0,0 +1,21 @@
+//To top button
+const toTop = document.querySelector(".to-top");
+toTop.addEventListener("click", toTopFunction);
+
+function toTopFunction() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+window.addEventListener("scroll", checkHeight);
+
+function checkHeight() {
+  if (window.scrollY > 100) {
+    toTop.style.display = "flex";
+  } else {
+    toTop.style.display = "none";
+  }
+}
